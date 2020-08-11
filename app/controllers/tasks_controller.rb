4ec49d59f7e475b7a12a9a228140fd794d43e855
Cara Bell <carabell1994@aim.com>
2020-08-11 15:24:27 +0100

    done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,45 @@
+# frozen_string_literal: true
+
+# this file is where we perform all our CRUD actions via methods
 class TasksController < ApplicationController
+  # see methodname.html.erb for the corresponding view
   def index
-    @tasks = Task.all
+    @tasks = Task.all # list all task instances
+  end
+
+  def show
+    @task = Task.find(params[:id]) # select the task by its ID
+  end
+
+  def new
+    @task = Task.new # create a new instance of task
+  end
+
+  def create
+    @task = Task.new(task_params) # we cannot save data without strong params - see private method below
+    @task.save # when creating, we always need to save
+    redirect_to task_path(@task) # to show we have updated, redirects back
+  end
+
+  def edit
+    @task = Task.find(params[:id]) # select the task by its ID
+  end
+
+  def update
+    @task = Task.find(params[:id]) # select task by its iD
+    @task.update(task_params) # update the task by selecting that specific task (with strong params too)
+    redirect_to task_path(@task) # to show we have updated, redirects back
+  end
+
+  def destroy
+    @task = Task.find(params[:id]) # select the task by its ID
+    @task.destroy
+    redirect_to tasks_path # to show we have updated, redirects back
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'tasks', to: 'tasks#index'
+  get 'tasks', to: 'tasks#index' # this is how we list all tasks
+  get 'tasks/new', to: 'tasks#new' # this is step 1 of creating a task (displaying the task form)
+  get 'tasks/:id', to: 'tasks#show', as: 'task' # this is how we list one task
+  post 'tasks', to: 'tasks#create' # this is step 2 of creating / adding a task
+  # i.e. (handling the post request generated from submitting the form)
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'tasks_edit' # step 1 of update method - getting the form
+  patch 'tasks/:id', to: 'tasks#update', as: 'tasks_update' # step 2 of update method - patching the form
+  delete 'tasks/:id', to: 'tasks#destroy' # delete an individual task
 end
